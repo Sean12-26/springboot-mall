2d97496464541491462ad850966b9efcbceda361
Sean12-26 <sean861226@gmail.com>
2024-03-05 12:00:59 +0800

    商品功能-Refactor category ，使用Enum類型

--- springboot-mall/src/main/java/com/seantong/springbootmall/model/Product.java
+++ springboot-mall/src/main/java/com/seantong/springbootmall/model/Product.java
@@ -1,13 +1,14 @@
 package com.seantong.springbootmall.model;
 
-import javax.swing.text.SimpleAttributeSet;
+import com.seantong.springbootmall.constant.ProductCategory;
+
 import java.util.Date;
 
 public class Product {
 
     private Integer productId;
     private String productName;
-    private String category;
+    private ProductCategory category;
     private String imageUrl;
     private Integer price;
     private Integer stock;
@@ -31,11 +32,11 @@ public class Product {
         this.productName = productName;
     }
 
-    public String getCategory() {
+    public ProductCategory getCategory() {
         return category;
     }
 
-    public void setCategory(String category) {
+    public void setCategory(ProductCategory category) {
         this.category = category;
     }
 
@@ -87,3 +88,4 @@ public class Product {
         this.lastModifiedDate = lastModifiedDate;
     }
 }
+
